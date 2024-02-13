91d0ff2929a8bacc4064f685755b27d2e82efa75
Fernando Haro Calvo <fernando@haroware.com>
2024-02-13 08:29:21 -0800

    Practica 1

---
+++ Practicas/Practica 1/P1.1.py
@@ -0,0 +1,7 @@
+precioProducto = 34.96
+
+descuento = precioProducto * 0.15
+
+precioConDescuento = precioProducto - descuento
+
+print(f"El precio del producto es: {precioConDescuento:.2f}")
---
+++ Practicas/Practica 1/P1.2.py
@@ -0,0 +1,7 @@
+x, h = 2, 5
+
+# Formula 1
+print(((1 / (x + h) ** 2) - (1 / x ** 2)) / h)
+
+# Formula 2
+print(-((2 * x + h) / ((x + h) ** 2 * x ** 2)))
